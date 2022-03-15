dd10faef40402cf4843d77ac9fdc9efd0e85302b
kodaichirou <kodaichi14@gmail.com>
2022-03-15 13:21:14 +0900

    I got it

--- nlp100_sec2/nlp100_12.py
+++ nlp100_sec2/nlp100_12.py
@@ -6,3 +6,7 @@ df = pd.read_csv('./popular-names.txt', header=None, sep='\t',
 col1 = df['name']
 col1.to_csv('./col1.txt', index=False)
 print(col1.head())#ncol output. standart is 5
+
+col1 = df['sex']
+col1.to_csv('./col2.txt', index=False)
+print(col1.head())#ncol output. standart is 5
